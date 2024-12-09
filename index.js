fb43d3a010bbd94dcc7c2b29daa9d7c3f3b52ff8
pgrmrmortuja <moutasimmortuja@gmail.com>
2024-12-10 00:53:41 +0600

    limited data send to client

--- index.js
+++ index.js
@@ -38,10 +38,10 @@ async function run() {
         })
 
         app.get('/equipments-limited', async (req, res) => {
-            const limit = parseInt(req.query.limit) || 7;
+            const limit = parseInt(req.query.limit) || 6;
             const cursor = equipmentCollection.find().limit(limit); 
             const result = await cursor.toArray(); 
-            res.send(result);
+            // res.send(result);
         });
 
         app.get('/equipments/:id', async (req, res) => {
