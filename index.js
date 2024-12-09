54999d7642dc7c9d86aad2e5facaaaaa7dba8bc6
pgrmrmortuja <moutasimmortuja@gmail.com>
2024-12-09 15:21:18 +0600

    server work

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dk8ve.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -26,16 +24,94 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
+       
         // await client.connect();
+
+        const database = client.db('equipmentDB');
+
+        const equipmentCollection = database.collection('equipment');
+
+        app.get('/equipments', async (req, res) => {
+            const cursor = equipmentCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/equipments-limited', async (req, res) => {
+            const limit = parseInt(req.query.limit) || 6;
+            const cursor = equipmentCollection.find().limit(limit); 
+            const result = await cursor.toArray(); 
+            res.send(result);
+        });
+
+        app.get('/equipments/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await equipmentCollection.findOne(query);
+            res.send(result);
+        })
+
+        app.get('/categories/:category', async(req, res) =>{
+            const category = req.params.category;
+            const filter = { category: category };
+            const result = await equipmentCollection.find(filter).toArray(); 
+            res.send(result);
+        })
+
+        app.get('/myEquipments/:email', async(req, res) =>{
+            const email = req.params.email;
+            const filter = { email: email };
+            const result = await equipmentCollection.find(filter).toArray(); 
+            res.send(result);
+        })
+
+    
+
+        app.post('/equipments', async (req, res) => {
+            const newEquipment = req.body;
+            console.log(newEquipment);
+
+            const result = await equipmentCollection.insertOne(newEquipment);
+            res.send(result);
+        })
+
+        app.put('/equipments/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true };
+            const update = req.body;
+            const equipment = {
+                $set: { 
+                    photo: update.photo,
+                    item: update.item,
+                    category: update.category,
+                    description: update.description,
+                    price: update.price,
+                    rating: update.rating,
+                    customization: update.customization,
+                    processing: update.processing,
+                    stockStatus: update.stockStatus,
+
+                }
+            }
+
+            const result = await equipmentCollection.updateOne(filter, equipment, options);
+            res.send(result);
+        })
+
+        app.delete('/equipments/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await equipmentCollection.deleteOne(query);
+            res.send(result);
+        })
 
         
 
-        // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
-        // Ensures that the client will close when you finish/error
+       
         // await client.close();
     }
 }
@@ -44,9 +120,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Coffee making server is running.')
+    res.send('Equipments server is running.')
 })
 
 app.listen(port, () => {
-    console.log(`Coffee Server is running on port: ${port}`);
+    console.log(`Equipments Server is running on port: ${port}`);
 })
